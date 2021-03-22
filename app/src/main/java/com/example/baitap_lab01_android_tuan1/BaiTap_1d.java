10785c5c8ecaaa4063525fcfd3281caf82400d8f
hung2504 <vanhung25042000@gmail.com>
2021-03-22 23:32:35 +0700

    commit c d

---
+++ app/src/main/java/com/example/baitap_lab01_android_tuan1/BaiTap_1d.java
@@ -0,0 +1,14 @@
+package com.example.baitap_lab01_android_tuan1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BaiTap_1d extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bai_tap_1d);
+    }
+}
